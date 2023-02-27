5302766f859e8d4c29502ac20064b42304b4b486
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-27 15:28:45 +0100

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import NewsPage from './components/NewsPage'
 
 function App() {
 
 
   return (
     <div>
-
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<NewsPage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,7 @@
+const NewsCard = () => {
+    return ( 
+        <h1>newscard</h1>
+     );
+}
+ 
+export default NewsCard;
