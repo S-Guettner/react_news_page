837fa322a05eb8f4ccf72b722cc8ac8accb0d023
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-27 21:36:46 +0100

    feat: added search and language input

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -5,18 +5,34 @@ import { v4 as uuidv4 } from 'uuid';
 const NewsPage = () => {
     
     const [news,setNews] = useState([])
-    
+    const [search,setSearch] = useState("tesla")
+    const [language,setLanguage] = useState("de")
+
     useEffect(() => {
-        fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=8d2aeaf407954f059f5ddf5ca56ab42c`)
+        fetch(`https://newsapi.org/v2/everything?q=${search}&language=${language}&apiKey=8d2aeaf407954f059f5ddf5ca56ab42c`)
         .then(response => response.json())
         .then(data => {
             setNews(data.articles)
         })
-    },[])
+    },[search , language ])
 
 
     return(
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 text-center">
+        <section>
+            <input onChange={(e)=> setSearch(e.target.value)} className='border-2 border-black' type="text" />
+
+            <select onChange={(e) => {setLanguage(e.target.value)}} name="languageInput" >
+                <option value="de">de</option>
+                <option value="en">en</option>
+                <option value="es">es</option>
+                <option value="fr">fr</option>
+                <option value="it">it</option>
+                <option value="nl">nl</option>
+                <option value="pl">pl</option>
+            </select>
+
+            
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 text-center overflow-x-hidden">
         {news.map((article) => {
             console.log((article.url).slice(0,10))
             
@@ -31,6 +47,7 @@ const NewsPage = () => {
         })}
 
         </div>
+        </section>
         )
 }
  
